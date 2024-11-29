8a4560cd0d4a17413170c9df1c38d15f5ded1688
omar hassan <omarhassan00123@gmail.com>
2024-11-29 20:16:52 +0200

    delete old

--- frontend/src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:3000" : "/api",
-	withCredentials: true, // send cookies to the server
-});
-
-export default axiosInstance;
